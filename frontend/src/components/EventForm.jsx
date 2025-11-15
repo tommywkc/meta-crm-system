883e075851524d9a8f29ea73e2f1281338beffbd
tommywkc <tommywkc@hotmail.com>
2025-11-15 21:41:41 +0800

    move dateFormatter.js to utils

--- frontend/src/components/EventForm.jsx
+++ frontend/src/components/EventForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { formatForDisplay } from '../styles/dateFormatter';
+import { formatForDisplay } from '../utils/dateFormatter';
 import { redTextStyle } from '../styles/TableStyles';
 import '../styles/BatchSessionStyles.css';
 
